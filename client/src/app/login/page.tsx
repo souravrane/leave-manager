2730d0189e338dc8e24decda1f99d4bc8688dc9e
Sourav Rane <SouravMohan.Rane@wisetechglobal.com>
2023-09-01 01:19:18 +0530

    login form

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,8 +1,45 @@
+"use client";
+
 import React from "react";
-import styles from "./Login.module.css";
+import styles from "./login.module.css";
+import TextField from "@components/TextField";
+import Typography from "@components/Typography";
+import Button from "@components/Button";
 
 const Login = () => {
-    return <div className={styles.main}></div>;
+    let emailAddress = "";
+    let password = "";
+
+    const updateEmail = () => {};
+    const updatePassword = () => {};
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.login}>
+                <Typography type="h2" className={styles.heading}>
+                    Log in to Leave Manager
+                </Typography>
+                <div className={styles.input_elements}>
+                    <TextField
+                        type="email"
+                        label="Email address"
+                        onChange={updateEmail}
+                    />
+                </div>
+                <div className={styles.input_elements}>
+                    <TextField
+                        type="password"
+                        label="Password"
+                        onChange={updatePassword}
+                    />
+                </div>
+
+                <div className={styles.btn}>
+                    <Button btnType="primary">Sign In</Button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
