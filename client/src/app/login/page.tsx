ebfda545ab9a898df3b05d158157deca333fed6e
Sourav Rane <SouravMohan.Rane@wisetechglobal.com>
2023-09-01 11:20:07 +0530

    content file created and auth setup

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,41 +1,54 @@
 "use client";
 
 import React from "react";
-import styles from "./login.module.css";
+import { useRouter } from "next/navigation";
+
+import Button from "@components/Button";
 import TextField from "@components/TextField";
 import Typography from "@components/Typography";
-import Button from "@components/Button";
+import { useGlobalAuthContext } from "@contextAPI/authContext";
+
+import Text from "./content.json";
+import styles from "./login.module.css";
 
 const Login = () => {
+    const { push } = useRouter();
+    const { setIsUserLoggedIn } = useGlobalAuthContext();
     let emailAddress = "";
     let password = "";
 
     const updateEmail = () => {};
     const updatePassword = () => {};
+    const signIn = () => {
+        setIsUserLoggedIn(true);
+        push("/dashboard");
+    };
 
     return (
         <div className={styles.container}>
             <div className={styles.login}>
                 <Typography type="h2" className={styles.heading}>
-                    Log in to Leave Manager
+                    {Text.heading}
                 </Typography>
                 <div className={styles.input_elements}>
                     <TextField
                         type="email"
-                        label="Email address"
+                        label={Text.labels.email}
                         onChange={updateEmail}
                     />
                 </div>
                 <div className={styles.input_elements}>
                     <TextField
                         type="password"
-                        label="Password"
+                        label={Text.labels.password}
                         onChange={updatePassword}
                     />
                 </div>
 
                 <div className={styles.btn}>
-                    <Button btnType="primary">Sign In</Button>
+                    <Button btnType="primary" onClick={signIn}>
+                        {Text.labels.loginBtn}
+                    </Button>
                 </div>
             </div>
         </div>
